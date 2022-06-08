376191c87ec4d75ae0ef30e921b19c10f650e6ad
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-06-08 10:36:40 -0500

    grep.py initial commit.

--- grep.py
+++ grep.py
@@ -5,6 +5,15 @@ import sys
 
 def countpatterninfile():
     regular_expression = input("Enter a regular expression: ")
+    file_handle = open('mbox-long.txt','r')
+
+    matches_found = 0
+    for line in file_handle:
+        matching_portion = re.findall(regular_expression, line)
+        if len(matching_portion) > 0:
+            matches_found += 1
+
+    print('mbox.txt had {} lines that matched {}'.format(matches_found, regular_expression))
 
 if __name__ == '__main__':
     # this is called a main method
